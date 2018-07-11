6a3165de5281814c31717a1f9de2aad21e4ed775
mleonardo93 <mleonardo@gmail.com>
2018-07-10 20:25:40 -0400

    user story 3 work

--- lib/kele.rb
+++ lib/kele.rb
@@ -19,4 +19,9 @@ class Kele
     response = self.class.get("https://www.bloc.io/api/v1/users/me", headers: {"authorization" => @token})
     JSON.parse(response.body)
   end
+
+  def get_mentor_availability(id)
+    response = self.class.get("https://www.bloc.io/api/v1/mentors/#{id}/student_availability", headers: {"authorization" => @token})
+    JSON.parse(response.body)
+  end
 end
