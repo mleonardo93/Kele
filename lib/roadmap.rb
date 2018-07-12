c19046959127b36f0255ab8720fbae0ff08a3e0e
mleonardo93 <mleonardo@gmail.com>
2018-07-11 22:29:07 -0400

    user story 7 work

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -8,4 +8,9 @@ module Roadmap
     response = self.class.get("https://www.bloc.io/api/v1/checkpoints/#{id}", headers: {"authorization" => @token})
     JSON.parse(response.body)
   end
+
+  def get_remaining_checkpoints(chain_id)
+    response = self.class.get("https://www.bloc.io/api/v1/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section", headers: {"authorization" => @token})
+    JSON.parse(response.body)
+  end
 end
